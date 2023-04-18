549e9ac719c472bf783669b84b366e4e6420c564
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-18 16:14:59 +0200

    created table

--- index.php
+++ index.php
@@ -53,59 +53,41 @@
 </head>
 <body>
     <main>
-        <?php foreach($hotels as $hotel) {
-
-            foreach($hotel as $key => $value) {
-                echo $value . '<br>';
-            }
-        }
-        ?>
+        
 
     <div class="container">
         <table class="table table-dark table-striped">
                 <thead>
                 <tr>
-                <th scope="col">#</th>
-                <?php foreach($hotels as $hotel) {
-
-                    foreach($hotel as $key => $value) {
-
-
-
-                        echo "'<th scope=\"col\">'" . $key; "'</th>'";
+                <?php 
+                    
                 
-                    }
+                foreach($hotels[0] as $key => $hotel) {
+
+                    
+                
+                    echo "'<th scope=\"col\">'" . $key; "'</th>'";
+
+                   
                 }
                 ?>
                 
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <th scope="row">1</th>
-
-                <?php foreach($hotels as $hotel) {
-
-                    foreach($hotel as $key => $value) {
-
-                        echo "'<td scope=\"col\">'" . $value; "'</td>'";
-
-                    }
+                
+            <?php foreach($hotels as $hotel) {
                     
+                echo "<tr>";
+                
+                foreach($hotel as $key => $value) {
+
+                    echo "'<td scope=\"col\">'" . $value; "'</td>'";
+
                 }
-                ?>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
+                echo "</tr>";
+            }
+            ?>
             </tbody>
         </table>
 
