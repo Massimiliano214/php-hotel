02bddc103d9c325e4a31db8df861c71a7c111751
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-18 15:39:00 +0200

    implemented bootstrap

--- index.php
+++ index.php
@@ -49,6 +49,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Selection</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <main>
@@ -59,6 +60,56 @@
             }
         }
         ?>
+
+    <div class="container">
+        <table class="table table-dark table-striped">
+                <thead>
+                <tr>
+                <th scope="col">#</th>
+                <?php foreach($hotels as $hotel) {
+
+                    foreach($hotel as $key => $value) {
+
+
+
+                        echo "'<th scope=\"col\">'" . $key; "'</th>'";
+                
+                    }
+                }
+                ?>
+                
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+
+                <?php foreach($hotels as $hotel) {
+
+                    foreach($hotel as $key => $value) {
+
+                        echo "'<td scope=\"col\">'" . $value; "'</td>'";
+
+                    }
+                    
+                }
+                ?>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
     </main>
 </body>
 </html>
